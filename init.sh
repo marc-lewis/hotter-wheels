8102a318aee459857885f95536141853e6ce483a
marc-lewis <marc@marclewis.io>
2019-08-15 20:13:29 +0100

    Add bluetooth pairing for xbox controller

---
+++ init.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# deps for approvxeng.input (approxeng.github.io/approxeng.input/bluetooth.html)
+sudo apt-get install python-dev python-pip gcc
+pip install approxeng.input
+
+# enable constant pairing with bluetooth controller
+# see approxeng.github.io/approxeng.input/bluetooth.html
+echo "options bluetooth disable_ertm=Y" | sudo tee -a /etc/modprobe.d/bluetooth.conf
+
+# bluetoothctl 
+# press pairing button on controller
+# pair C8:3F:26:36:53:63
+# press pairing button on controller
+# connect C8:3F:26:36:53:63
